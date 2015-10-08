6d9fe66d81d7032da9b14887218da1f3c51795e9
Fabrice Bellingard <fabrice.bellingard@sonarsource.com>
2015-10-08 15:30:51 +0200

    Show marker on Map

--- src/test/resources/locations.js
+++ src/test/resources/locations.js
@@ -6,7 +6,7 @@
 function initMap() {
   var map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: -34.397, lng: 150.644},
-    zoom: 16
+    zoom: 19
   });
   var infoWindow = new google.maps.InfoWindow({map: map});
 
@@ -19,8 +19,11 @@ function initMap() {
       };
 
       infoWindow.setPosition(pos);
-      infoWindow.setContent('Current: ' + position.coords.latitude + ' / ' + position.coords.longitude);
+      infoWindow.setContent('BDD Room');
+
       map.setCenter(pos);
+
+      fetchUserLocations(map);
     }, function() {
       handleLocationError(true, infoWindow, map.getCenter());
     });
@@ -34,5 +37,31 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
   infoWindow.setPosition(pos);
   infoWindow.setContent(browserHasGeolocation ?
                         'Error: The Geolocation service failed.' :
-                        'Error: Your browser doesn\'t support geolocation.');
+                        'Error: Your browser does not support geolocation.');
+}
+
+function fetchUserLocations(map) {
+
+  var req = new XMLHttpRequest();
+  req.onreadystatechange = function() {
+      if (req.readyState == 4 && req.status == 200) {
+          showLocations(map, req.responseText);
+      }
+  }
+  req.open("GET", "https://sportstalker.apispark.net/v1/users/", true);
+  req.setRequestHeader("Authorization", "Basic Y2VkMTlkYzctZGE1Zi00N2ViLWJiYzAtZmM0YTNjNjZjNjZiOjhkNzIwNWM4LTBmNGItNDE0Yy1iMDBiLWE5NWZmMWViYjA5Mw==");
+  req.send(null);
+}
+
+function showLocations(map, responseText) {
+  var users = JSON.parse(responseText);
+  users.forEach(function(user) {
+    var marker = new google.maps.Marker({
+      map: map,
+      position: {lat: parseFloat(user.latitude), lng: parseFloat(user.longitude)},
+      icon: {url: 'mini_icon_run.png'},
+      title: user.username
+    });
+    marker.setAnimation();
+  });
 }
